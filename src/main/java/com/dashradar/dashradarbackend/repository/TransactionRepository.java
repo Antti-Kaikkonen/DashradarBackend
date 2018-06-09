eb04584f20b62f1a7a403e527d5c8f0907a00c9f
Antti Kaikkonen <35118741+Antti-Kaikkonen@users.noreply.github.com>
2018-06-09 09:37:21 +0300

    added MIXING_SOURCE and FIRST_ROUND connections back. Mempool importing

--- src/main/java/com/dashradar/dashradarbackend/repository/TransactionRepository.java
+++ src/main/java/com/dashradar/dashradarbackend/repository/TransactionRepository.java
@@ -30,11 +30,10 @@ public interface TransactionRepository extends Neo4jRepository<Transaction, Long
     )
     List<String> getMempoolTxids();
     
-    @Query("MATCH (mempool:Mempool) "+
+    @Query("MERGE (mempool:Mempool) "+
            "CREATE (tx:Transaction)-[:INCLUDED_IN]->(mempool) "+
-           "SET tx = {n: {0}, locktime: {1}, pstype: {2}, size: {3}, txid: {4}, version: {5}};")
-    void createMempoolTransaction(int n, 
-            long locktime, 
+           "SET tx = {locktime: {0}, pstype: {1}, size: {2}, txid: {3}, version: {4}};")
+    void createMempoolTransaction(long locktime, 
             int pstype, 
             long size, 
             String txid, 
@@ -68,9 +67,28 @@ public interface TransactionRepository extends Neo4jRepository<Transaction, Long
     )
     void create_previous_connections(String blockhash, int pstype);
     
-    @Query("MATCH (tx:Transaction {txid:{0}})-[r:INCLUDED_IN]->(mempoo:Mempool) "+
-           "DELETE r WITH tx MATCH (block:Block {hash:{1}}) CREATE (tx)-[:INCLUDED_IN]->(block);")
-    void moveMempooTransactionToBlock(String txid, String blockHash);
+    @Query(
+        "MATCH (b:Block {hash:{0}})<-[:INCLUDED_IN]-(tx1:Transaction {pstype:{1}})<-[:INPUT]-(i:TransactionInput)<-[:SPENT_IN]-(o:TransactionOutput)<-[:OUTPUT]-(tx2:Transaction {pstype:"+Transaction.PRIVATE_SEND_CREATE_DENOMINATIONS+"})\n" +
+        "WITH tx1,tx2, count(o) as rel_count\n" +
+        "CREATE (tx1)-[:MIXING_SOURCE {connections:rel_count}]->(tx2);"
+    )
+    void create_mixing_source_connections(String blockhash, int pstype);
+    
+    @Query(
+        "MATCH (b:Block {hash:{0}})<-[:INCLUDED_IN]-(tx1:Transaction {pstype:"+Transaction.PRIVATE_SEND+"})<-[:INPUT]-(i:TransactionInput)<-[:SPENT_IN]-(o:TransactionOutput)<-[:OUTPUT]-(tx2:Transaction {pstype:{1}})\n" +
+        "WITH tx1,tx2, count(o) as rel_count\n" +
+        "CREATE (tx1)-[:FIRST_ROUND {connections:rel_count}]->(tx2);"
+    )
+    void create_first_round_connections(String blockhash, int pstype);
+    
+    @Query(
+        "MATCH (tx:Transaction {txid:{0}})-[r:INCLUDED_IN]->(mempoo:Mempool) "+
+        "SET tx.n = {2} "+        
+        "DELETE r "+
+        "WITH tx "+
+        "MATCH (block:Block {hash:{1}}) CREATE (tx)-[:INCLUDED_IN]->(block);"
+    )
+    void moveMempooTransactionToBlock(String txid, String blockHash, int n);
     
     @Query(
             "MATCH (:Block {height:{0}})<-[:INCLUDED_IN]-(tx:Transaction) RETURN tx.txid ORDER BY tx.n;"
