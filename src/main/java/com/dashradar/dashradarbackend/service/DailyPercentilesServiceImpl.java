cb9cf20f34fefc2d0a096dc5397d2381a341198a
Antti Kaikkonen <35118741+Antti-Kaikkonen@users.noreply.github.com>
2018-11-30 07:14:40 +0200

    added debugging

--- src/main/java/com/dashradar/dashradarbackend/service/DailyPercentilesServiceImpl.java
+++ src/main/java/com/dashradar/dashradarbackend/service/DailyPercentilesServiceImpl.java
@@ -19,22 +19,38 @@ public class DailyPercentilesServiceImpl implements DailyPercentilesService {
     @Override
     @Transactional
     public void createDailyPercentiles(long day, double percentile) {
+        System.out.println("Creating daily percentiles for day "+day+" and percentile "+percentile);
+        System.out.println("createDailyFeePerByteSat");
         dailyPercentilesRepository.createDailyFeePerByteSat(day, percentile);
+        System.out.println("createDailyFeeSat");
         dailyPercentilesRepository.createDailyFeeSat(day, percentile);
+        System.out.println("createPrivateSendFeePerByteSat");
         dailyPercentilesRepository.createPrivateSendFeePerByteSat(day, percentile);
+        System.out.println("createPrivateSendFeeSat");
         dailyPercentilesRepository.createPrivateSendFeeSat(day, percentile);
+        System.out.println("createCreateDenominationsFeePerByteSat");
         dailyPercentilesRepository.createCreateDenominationsFeePerByteSat(day, percentile);
+        System.out.println("createCreateDenominationsFeeSat");
         dailyPercentilesRepository.createCreateDenominationsFeeSat(day, percentile);
+        System.out.println("createRegularTransactionFeePerByteSat");
         dailyPercentilesRepository.createRegularTransactionFeePerByteSat(day, percentile);
+        System.out.println("createRegularTransactionFeeSat");
         dailyPercentilesRepository.createRegularTransactionFeeSat(day, percentile);
         
         dailyPercentilesRepository.create_tx_size(day, percentile);
+        System.out.println("create_privatesend_tx_size_size");
         dailyPercentilesRepository.create_privatesend_tx_size_size(day, percentile);
+        System.out.println("create_regular_tx_size_size");
         dailyPercentilesRepository.create_regular_tx_size_size(day, percentile);
+        System.out.println("create_mixing_10_0_tx_size_size");
         dailyPercentilesRepository.create_mixing_10_0_tx_size_size(day, percentile);
+        System.out.println("create_mixing_1_0_tx_size_size");
         dailyPercentilesRepository.create_mixing_1_0_tx_size_size(day, percentile);
+        System.out.println("create_mixing_0_1_tx_size_size");
         dailyPercentilesRepository.create_mixing_0_1_tx_size_size(day, percentile);
+        System.out.println("create_mixing_0_01_tx_size_size");
         dailyPercentilesRepository.create_mixing_0_01_tx_size_size(day, percentile);
+        System.out.println("create_create_denominations_tx_size");
         dailyPercentilesRepository.create_create_denominations_tx_size(day, percentile);
     }
     
